bb4b8b93ba7a6d12348afa80bba5af30398d0059
root <alexdelei125@gmail.com>
2024-05-28 06:48:27 +0000

    issues idk from where

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,6 +3,7 @@ function getCurrentYear() {
   return date.getFullYear();
 }
 
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const currentYear = getCurrentYear();
   const budget = {
