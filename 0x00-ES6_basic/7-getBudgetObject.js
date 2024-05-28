90556cf403a0ae2f261c47c77af52084cc4808b7
root <alexdelei125@gmail.com>
2024-05-28 04:56:53 +0000

    JS Object ShortHand Syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,13 @@
+/**
+ * Shorthand syntax: keynames that represent object property value
+ * getBudgetObject - Object shorthand syntax
+ */
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
