c53b6bf0251bc13cb31d6334a4ddb72751d040ba
root <alexdelei125@gmail.com>
2024-05-28 04:20:29 +0000

    spread operator

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,14 @@
+/**
+ * Using the spread syntax
+ * Its expands an iterable
+ *
+ * concatArrays - concatinate two arrays and a string into a single array
+ * @array1 - first array
+ * @array2 - second array
+ * @string - a word
+ *
+ * return: an array of concatination
+ */
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string]
+}
