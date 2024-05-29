0aa50b2d1b4c8cde330c0937623a4f3276e11a27
AlexDelei <alexdelei125@gmail.com>
2024-05-29 12:50:31 +0300

    Object property method

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+/**
+ * createReportObject - object with goodstuff
+ * @param { the return values of the previous function } employeesList 
+ * @returns  an object with keys that return data
+ */
+export default function createReportObject(employeesList) {
+  const allEmployees = {...employeesList};
+ 
+  const getNumberOfDepartments = (data) => {
+    return Object.keys(data).length
+  }
+
+  return {allEmployees, getNumberOfDepartments};
+}
