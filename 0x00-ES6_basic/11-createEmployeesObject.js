f30f309eadfaeabc601081819f0b14c0d9a7d3d7
AlexDelei <alexdelei125@gmail.com>
2024-05-29 09:14:26 +0300

    comment

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,9 @@
+/**
+ * createEmployeesObject - return an object with department name and employees
+ * @param {*} departmentName 
+ * @param {*} employees 
+ * @returns an objects with employees
+ */
 export default function createEmployeesObject(departmentName, employees) {
   return {
     [`${departmentName}`]: employees
