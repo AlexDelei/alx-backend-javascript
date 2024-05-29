a493e731ac32687ada1e23d2f713ed2b9e24bb3c
AlexDelei <alexdelei125@gmail.com>
2024-05-29 13:54:15 +0300

    fixed coding style issues

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,12 +1,12 @@
 /**
  * createEmployeesObject - return an object with department name and employees
- * @param {*} departmentName 
- * @param {*} employees 
+ * @param {*} departmentName
+ * @param {*} employees
  * @returns an objects with employees
  */
 
 export default function createEmployeesObject(departmentName, employees) {
   return {
-    [`${departmentName}`]: employees
+    [`${departmentName}`]: employees,
   };
 }
