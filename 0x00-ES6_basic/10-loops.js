fcae725b5144c4e330a8d088ad9d3a107eb35dbd
AlexDelei <alexdelei125@gmail.com>
2024-05-29 13:52:57 +0300

    Haved added comments and fixed issues

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,14 @@
+/**
+ * appendToEachArrayValue - appending a string to the passed array
+ * @param {*} array
+ * @param {*} appendString
+ * @returns - the modified array
+ */
 export default function appendToEachArrayValue(array, appendString) {
   const modified = [];
   for (const val of array) {
     modified.push(appendString + val);
-}
+  }
 
   return modified;
 }
