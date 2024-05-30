52e9a46ff3d0c15597d81899efab595bfaebc090
AlexDelei <alexdelei125@gmail.com>
2024-05-30 08:00:17 +0300

    resolve a promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,17 +1,9 @@
+/**
+ * signUpUser - resolve a promise and return an object
+ * @param {*} firstName 
+ * @param {*} lastName 
+ * @returns 
+ */
 export default function signUpUser(firstName, lastName) {
-  const whatsThatBrother = new Promise((resolve, reject) => {
-    if (firstName && lastName) {
-      resolve({ firstName, lastName });
-    } else {
-      reject('Name Cannot be blank');
-    }
-  });
-
-  return whatsThatBrother
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(() => {
-      console.log(new Error(''));
-    });
+  return Promise.resolve({ firstName, lastName });
 }
