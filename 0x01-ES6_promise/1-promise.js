c5d03c7001af6001b57b33336a53cc89d588539d
AlexDelei <alexdelei125@gmail.com>
2024-05-30 05:28:21 +0300

    promise with an object

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const obj = {
+        status: 200,
+        body: 'success',
+      };
+      if (success) {
+        resolve(obj);
+      } else {
+        reject(new Error(['The fake API is not working currently']));
+      }
+    }, 100);
+  });
+}
