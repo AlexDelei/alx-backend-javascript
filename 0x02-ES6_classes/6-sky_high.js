bc278de012ed75450475e8b4a6e8f1c6720845ec
AlexDelei <alexdelei125@gmail.com>
2024-05-31 23:13:56 +0300

    inheriting

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,8 +1,8 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
+  constructor(sqrt, floors) {
+    super(sqrt);
     this._floors = floors;
   }
 
@@ -15,6 +15,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
