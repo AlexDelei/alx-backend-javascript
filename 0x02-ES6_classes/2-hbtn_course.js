45625e4f85500e22f0f9a13137a56e4c4babf557
AlexDelei <alexdelei125@gmail.com>
2024-05-31 15:46:51 +0300

    getter and setter functions

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    this._students = val;
+  }
+}
