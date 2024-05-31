5aa56fcb0f9e60f4f050528112d98772d5f0521e
AlexDelei <alexdelei125@gmail.com>
2024-05-31 14:51:05 +0300

    initializing with constructor

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+/**
+ * ClassRoom - initializing with the passed parameter
+ */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
