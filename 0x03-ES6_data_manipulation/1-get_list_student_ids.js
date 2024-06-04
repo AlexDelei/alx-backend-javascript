6c08172452e961fc9658b09b30fb33e9e8c4a884
AlexDelei <alexdelei125@gmail.com>
2024-06-04 19:43:38 +0300

    first use of map function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,8 @@
+/**
+ * getListStudentIds - map > constructs a new array after iterating
+ * @param {an array of objects} arr 
+ * @returns 
+ */
 export default function getListStudentIds(arr) {
   if (!Array.isArray(arr)) return [];
   const newArr = arr.map((x) => x.id);
