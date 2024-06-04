119a9f632edf65f42cbe50db81e1a8c778fa728a
AlexDelei <alexdelei125@gmail.com>
2024-06-04 19:46:08 +0300

    commentingg

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,7 @@
+/**
+ * getListStudents - constructing an array of objects
+ * @returns an array of objects
+ */
 export default function getListStudents() {
   const x = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const y = { id: 2, firstName: 'James', location: 'Columbia' };
