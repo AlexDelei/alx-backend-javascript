e84f115d3ee69064ae94015d1e09835fed641eb6
AlexDelei <alexdelei125@gmail.com>
2024-06-04 22:17:35 +0300

    set with data from an array object, documentation is a must

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,8 @@
+/**
+ * setFromArray - creates a set with data from an array
+ * @param {array} arr
+ * @returns a set of objects
+ */
 export default function setFromArray(arr) {
   const setArr = new Set(Object.values(arr));
 
