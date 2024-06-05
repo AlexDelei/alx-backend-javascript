70367909a62d99a82848ec263b28592626732e58
AlexDelei <alexdelei125@gmail.com>
2024-06-05 06:14:06 +0300

    Create a new weakmap object

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,9 @@
 export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const queries = weakMap.get(endpoint);
     if (queries >= 4) throw Error('Endpoint load is high');
-    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    weakMap.set(endpoint, queries + 1);
   } else { weakMap.set(endpoint, 1); }
 }
