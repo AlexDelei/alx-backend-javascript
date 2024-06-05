7b74e0cdab70ae4e163c7bd74eac2bf1278c2d32
AlexDelei <alexdelei125@gmail.com>
2024-06-05 05:55:39 +0300

    a new map function, documentation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,3 +1,8 @@
+/**
+ * updateUniqueItems - a new map object
+ * @param {a map object} map
+ * @returns 
+ */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw Error('Cannot process');
   for (const j of map) if (j[1] === 1) map.set(j[0], 100);
