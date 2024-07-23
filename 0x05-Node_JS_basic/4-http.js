153021bbd63bba96bc7132f63af226a88907c258
AlexDelei <alexdelei125@gmail.com>
2024-07-23 15:34:33 +0300

    creating a simple server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 1245;
 const app = createServer((req, res) => {
   res.statusCode = 200;
