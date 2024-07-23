e60fb891341aba5a41380d2862f925174f5658d9
AlexDelei <alexdelei125@gmail.com>
2024-07-23 15:06:23 +0300

    Reading a file asychronously with node js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const students = data.trim().split('\n').slice(1);
+    console.log(`Number of students: ${students.length}`);
+
+    const firstName = [];
+    const sweNames = [];
+    let cs = 0;
+    let swe = 0;
+
+    for (let i = 0; i < students.length; i += 1) {
+      const field = students[i].split(',').slice(-1)[0].trim();
+
+      if (field === 'CS') {
+        cs += 1;
+        const names = students[i].split(',')[0].trim();
+        firstName.push(names);
+      } else if (field === 'SWE') {
+        swe += 1;
+        const names = students[i].split(',')[0].trim();
+        sweNames.push(names);
+      }
+    }
+
+    console.log(`Number of students in CS: ${cs}. List: ${firstName.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe}. List: ${sweNames.join(', ')}`);
+  } catch (error) {
+    console.error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
