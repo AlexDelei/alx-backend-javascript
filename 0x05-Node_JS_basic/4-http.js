fd49fca801622fe2b588755c935c3cfb60d05e2a
AlexDelei <alexdelei125@gmail.com>
2024-07-23 15:31:44 +0300

    creating a simple server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,8 +6,6 @@ const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-});
-
-app.listen(port, hostname);
+}).listen(port, hostname);
 
 module.exports = app;
