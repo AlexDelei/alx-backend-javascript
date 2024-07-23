fcd7b569702cb1163cf4fef0a4573cb6322f8737
AlexDelei <alexdelei125@gmail.com>
2024-07-23 16:31:15 +0300

    Creating a more complex http server

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245;
+
+const app = createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    try {
+      const result = await countStudents('database.csv');
+      res.write(result);
+    } catch (error) {
+      res.write('Cannot load the database');
+    }
+    res.end();
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
