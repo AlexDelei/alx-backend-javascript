75eccc27962d9bd0a171600566a8d59bf8b23c1f
AlexDelei <alexdelei125@gmail.com>
2024-07-24 07:34:49 +0300

    added the return status codes and proper error handling

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,16 +4,17 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 
 const app = createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
     res.end();
   } else if (req.url === '/students') {
     try {
       const result = await countStudents(process.argv[2]);
+      res.statusCode = 200
       res.write(`This is the list of our students\n${result}`);
     } catch (error) {
+      res.statusCode = 500
       res.write('Cannot load the database');
     }
     res.end();
