4fc42def31130a50eaed56b97c4b3eba9dba7df1
AlexDelei <alexdelei125@gmail.com>
2024-07-24 06:39:42 +0300

    retreived the correct param from the url

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -18,14 +18,14 @@ class StudentsController {
   }
 
   static async getAllStudentsByMajor(request, response) {
-    const major = request.query.param1;
+    const major = request.params.major;
 
     if (major === 'CS') {
       try {
         const csData = await readDatabase('database.csv');
         const results = csData[0].CS;
         response.statusCode = 200;
-        response.send(results);
+        response.send(`List: ${results}`);
       } catch (error) {
         response.statusCode = 500;
         response.send('Cannot load the database');
@@ -33,16 +33,16 @@ class StudentsController {
     } else if (major === 'SWE') {
       try {
         const sweData = await readDatabase('database.csv');
-        const resultss = sweData[1].SWE;
+        const resultss = sweData[0].SWE;
         response.statusCode = 200;
-        response.send(resultss);
+        response.send(`List: ${resultss}`);
       } catch (error) {
         response.statusCode = 500;
         response.send('Cannot load the database');
       }
     } else {
       response.statusCode = 500;
-      response.send('Major parameter must be CS or SWE');
+      response.send('Major parameter must be CS or SWE\n');
     }
     response.end();
   }
