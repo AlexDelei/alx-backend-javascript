1e16ffd22cc85904bc256fd7bc85cd3135407618
AlexDelei <alexdelei125@gmail.com>
2024-07-25 10:14:16 +0300

    Basic test with Mocha

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,15 +1,13 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('my first Test Case in JavaScript', () => {
-  it('tests add function', () => {
-    assert.equal(calculateNumber(1, 3.8), 5);
-    assert.notEqual(calculateNumber(1, 3.9), 4.9);
-    assert.equal(calculateNumber(1.3, 4.5), 6);
-    assert.notEqual(calculateNumber(1.3, 4.5), 5.8);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-    assert.notEqual(calculateNumber(1.5, 3.7), 5.2);
-    assert.strictEqual(calculateNumber(10, 25), 35);
-    assert.notStrictEqual(calculateNumber(10, 25), 45);
+describe('my first test case in js', () => {
+  it('should add two numbers', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.5), 4);
+    assert.strictEqual(calculateNumber(1.6, 2.4), 4);
+    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
+  });
+  it('should handle test cases', () => {
+    assert.strictEqual(calculateNumber(0.5, 0.5), 2);
   });
 });
