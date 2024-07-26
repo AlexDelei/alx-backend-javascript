3698a114434377c4031bc6a29a76972ef7607a2f
AlexDelei <alexdelei125@gmail.com>
2024-07-26 09:26:07 +0300

    Same calculator as on task 1

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,17 @@
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    const res = Math.round(a) / Math.round(b);
+    return res;
+  }
+};
+
+module.exports = calculateNumber;
