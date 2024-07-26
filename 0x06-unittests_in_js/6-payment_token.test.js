312f948fef14db182c4539751243f7e6be8af51f
AlexDelei <alexdelei125@gmail.com>
2024-07-26 11:33:34 +0300

    async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,11 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('get payment token from api', () => {
-  it('should return a successful response when called with true', () => {
-    getPaymentTokenFromAPI(true).then((response) => {
+  it('should return a successful response when called with true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
         expect(response).to.deep.equal({ data: 'Successful response from the API' });
         done();
-      });
+      }).catch((err) => done(err));
   });
 });
