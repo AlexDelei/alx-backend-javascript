511e351e7dbdf5f8d9f998d110ea4f96409a5040
AlexDelei <alexdelei125@gmail.com>
2024-07-26 18:24:09 +0300

    Regex integration testig

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  if (!req.params.id) {
+    return res.send('id cannot be empty');
+  }
+  const id = parseInt(req.params.id, 10);
+  if (isNaN(id)) {
+    return res.status(404).send('id must be an integer');
+  }
+  res.status(200).send(`Payment methods for cart ${id}`);
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
