cee11a1ef39bfa30eec4f49b34800aefabfc6968
AlexDelei <alexdelei125@gmail.com>
2024-07-26 17:34:46 +0300

    testing if the route exists

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,9 +6,20 @@ const expect = chai.expect;
 describe('a one suite test for the index page', () => {
   it('performs an integration to our route /', () => {
     req('http://localhost:7865/', 'GET', (error, respo, body) => {
-      if (error) throw error;
       expect(respo.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
+    });
+  });
+
+  it('checks if the GET / exists', () => {
+    req('http://localhost:7865/', 'GET', (err, respo, body) => {
+      expect(respo.statusCode).to.be.equal(200);
+    });
+  });
+
+  it('checks for a non-existing route', () => {
+    req('http://localhost:7865/home', 'GET', (err, respo, body) => {
+      expect(respo.statusCode).to.be.equal(404);
     });
   });
 });
