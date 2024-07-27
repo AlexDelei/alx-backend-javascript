2d65d61e70192e016136288e93fe7890a0e81dc9
AlexDelei <alexdelei125@gmail.com>
2024-07-27 18:07:26 +0300

    added two routes

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+// Enable middle to parse content-type: application/json
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (!(id) || isNaN(id) || id < 0) {
+    return res.status(404).send('An error occured while trying to make your request');
+  };
+  res.status(200).send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  res.status(200).send(
+    {
+      'payment_methods': {
+        'credit_cards': true,
+        'paypal': false,
+      },
+    }
+  );
+});
+
+app.post('/login', (req, res) => {
+  res.send(`Welcome ${req.body.userName}`);
+})
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
